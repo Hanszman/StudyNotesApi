ddd7e4f995ca056f972db6bcece40befba68a41b
Hanszman <victor.hanszman@hotmail.com>
2026-04-11 10:31:33 -0300

    creating entities

--- src/StudyNotesApi.Api/Program.cs
+++ src/StudyNotesApi.Api/Program.cs
@@ -5,6 +5,7 @@ EnvironmentFileLoader.LoadFromSolutionRoot();
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,6 +24,7 @@ app.UseAuthorization();
 app.MapGet("/", () => Results.Redirect("/swagger"))
     .ExcludeFromDescription();
 
+app.MapHealthChecks("/api/health");
 app.MapControllers();
 
 app.Run();
